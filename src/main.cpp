3c81bed8eb8c1d7e7db20a8203e8e558c68e024e
marcus <marcus@r38.se>
2025-04-15 18:35:08 +0200

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ int main() {
 
     spdlog::info("Info from spdlog");
     spdlog::error("Error from spdlog");
-
+    spdlog::debug("This is a debug message");
+    spdlog::warn("{:>30}", "right aligned by 30");
     return 0;
 }
